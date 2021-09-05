1e3c5e3f491167e9e1e1c1b9972c40604cb73ac3
favio <faugconti@gmail.com>
2021-09-05 02:19:59 -0300

    fixed firebase data, changed real data for private envs

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import firebase from "firebase/app";
+import "firebase/auth"; // for authentication
+import "firebase/storage"; // for storage
+import "firebase/database"; // for realtime database
+import "firebase/firestore"; // for cloud firestore
+import "firebase/messaging"; // for cloud messaging
+import "firebase/functions"; // for cloud functions
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
